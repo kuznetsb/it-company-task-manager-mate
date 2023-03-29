2b0a57da276d3c64092ac41ef60ac2e6c2ff495c
kuznetsb <kuznetsbog@gmail.com>
2023-03-29 17:35:31 +0300

    Changed for plural forms in url path

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,23 +16,23 @@ urlpatterns = [
     path("", index, name="index"),
     path("positions/", PositionListView.as_view(), name="position-list"),
     path(
-        "position/create/",
+        "positions/create/",
         PositionCreateView.as_view(),
         name="position-create"
     ),
     path(
-        "position/<int:pk>/delete",
+        "positions/<int:pk>/delete",
         PositionDeleteView.as_view(),
         name="position-delete"
     ),
     path("task_types/", TaskTypeListView.as_view(), name="task-type-list"),
     path(
-        "task_type/create/",
+        "task_types/create/",
         TaskTypeCreateView.as_view(),
         name="task-type-create"
     ),
     path(
-        "task_type/<int:pk>/delete/",
+        "task_types/<int:pk>/delete/",
         TaskTypeDeleteView.as_view(),
         name="task-type-delete"
     ),
