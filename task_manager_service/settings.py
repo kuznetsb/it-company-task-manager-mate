cfddd3783502f1be373254ba156a725a2765d5df
kuznetsb <kuznetsbog@gmail.com>
2023-03-29 15:24:03 +0300

    Added django auth to project and made basic login/logout templates

--- task_manager_service/settings.py
+++ task_manager_service/settings.py
@@ -109,6 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "task_manager.Worker"
 
+LOGIN_REDIRECT_URL = "/"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
--- task_manager_service/urls.py
+++ task_manager_service/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("task_manager.urls", namespace="manager")),
-    path('__debug__/', include('debug_toolbar.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("accounts/", include("django.contrib.auth.urls"))
 ]
